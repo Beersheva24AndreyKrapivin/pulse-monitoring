3f73bb46b37ee7e36a38688e72622ea6db80a8b2
Андрей Крапивин <without.a.body@gmail.com>
2025-03-26 11:39:47 +0200

    sdk

--- data-provisioning-source/src/main/java/telran/monitoring/DataSource.java
+++ data-provisioning-source/src/main/java/telran/monitoring/DataSource.java
@@ -1,6 +1,8 @@
 package telran.monitoring;
 
+import telran.monitoring.logging.Logger;
+
 public interface DataSource {
     String getData(long patientId);
-    static Logger [] loggers = new 
+    static Logger [] loggers = new Logger[1];
 }
